091a40292d03ae92efffa8ddd314bac9674456de
withoutaface <43731789+withoutaface@users.noreply.github.com>
2021-04-19 22:48:40 +0200

    Initial commit

---
+++ ImGuiMonoGame/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ImGuiMonoGame")]
+[assembly: AssemblyProduct("ImGuiMonoGame")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8854d955-f90f-44f5-a1df-48cb5f684790")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,12 @@
+# MonoGame ImGui.NET examples
+
+![Alt text](image/monogameimguinet.png "Example Image")
+
+Features:
+- Reimplemented demo windows from imgui_demo.cpp to c#
+- Log, Console, Overlay
+- Simple MonoGame example window
+
+Requirements:
+- MonoGame 3.7
+- Visual Studio 2019
